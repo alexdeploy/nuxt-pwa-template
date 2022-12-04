571fbd8a6fe17ca779ffad52724058002e92517d
alexruedadev <me@alexrueda.dev>
2022-12-04 01:04:16 +0100

    Added basic nuxt config files

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+/*
+* Nuxt 3 Config File
+* https://nuxt.com/docs/getting-started/configuration#app-configuration
+*/
+export default defineAppConfig({
+    title: 'Hello Nuxt',
+    theme: {
+      dark: true,
+      colors: {
+        primary: '#ff0000'
+      }
+    }
+  })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+/*
+ * Nuxt 3 Config File
+ * https://nuxt.com/docs/api/configuration/nuxt-config
+ */
+// 
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+        // The private keys which are only available server-side
+        apiSecret: '123',
+        // Keys within public are also exposed client-side
+        public: {
+          apiBase: '/api'
+        }
+    }
 })
