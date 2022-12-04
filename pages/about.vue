78c1c2bf0132953610eb49c4dee90ca08dfb4946
alexruedadev <me@alexrueda.dev>
2022-12-04 01:34:24 +0100

    Basic directories added

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>About page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+      <h1>Welcome to the homepage</h1>
+      <AppAlert>
+        This is an auto-imported component
+      </AppAlert>
+    </div>
+  </template>
