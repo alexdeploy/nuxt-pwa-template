a7f752b6ed37c6f9643cdb707e5a56d3580fbe74
alexruedadev <me@alexrueda.dev>
2022-12-04 14:46:45 +0100

    Tailwind install and basic config for tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,39 @@
 /*
  * Nuxt 3 Config File
- * https://nuxt.com/docs/api/configuration/nuxt-config
+ Usage: https://nuxt.com/docs/api/configuration/nuxt-config
  */
 // 
 export default defineNuxtConfig({
-    runtimeConfig: {
-        // The private keys which are only available server-side
-        apiSecret: '123',
-        // Keys within public are also exposed client-side
-        public: {
-          apiBase: '/api'
-        }
+
+  /**
+   * * Nuxt 3 Modules
+   * All modules: https://nuxt.com/modules
+   */
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  /**
+   * * Tailwind CSS Config
+   * Options: https://tailwindcss.nuxt.dev/getting-started/options/
+   */
+  tailwindcss: {
+    cssPath: '~/assets/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
+
+  /**
+   * * Runtime Config (Environment Variables)
+   * Usage: https://nuxt.com/docs/guide/going-further/runtime-config
+   */
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api'
     }
+  }
 })
