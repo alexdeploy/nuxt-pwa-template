86da1acc51e75b11d3d1a8e4169ca483a5c68a00
alexruedadev <me@alexrueda.dev>
2022-12-04 16:58:11 +0100

    Added some basic styles to header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,11 +1,22 @@
 <template>
-    <header>
-      <nav>
-        <ul>
+    <header class="sticky top-0 z-10 backdrop-blur-md">
+      <nav class="navigation justify-between">
+        <img src="favicon.ico" alt="" class="w-10 h-10">
+        
+        <ul class="hidden space-x-5 items-center lg:flex">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
         </ul>
+
+        <img src="/img/menu.svg" alt="" class="flex lg:hidden">
       </nav>
     </header>
   </template>
   
+<style>
+.navigation {
+    @apply flex;
+    @apply p-5;
+    @apply space-x-5;
+  }
+</style>
