d51400076093da984d30a1ed8445261f7ac560f6
alexruedadev <me@alexrueda.dev>
2022-12-05 20:04:23 +0100

    Added nuxt link to home hover logo

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
     <header class="sticky top-0 z-10 backdrop-blur-md">
       <nav class="navigation justify-between">
-        <img src="favicon.ico" alt="" class="w-10 h-10">
+        <NuxtLink to="/">
+          <img src="favicon.ico" alt="" class="w-10 h-10">
+        </NuxtLink>
         
         <ul class="hidden space-x-5 items-center lg:flex">
           <li><NuxtLink to="/">Home</NuxtLink></li>
