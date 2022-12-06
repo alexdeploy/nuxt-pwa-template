3ae7bc4382cad74e6ec42af104cc86c23580901c
alexruedadev <me@alexrueda.dev>
2022-12-06 02:57:52 +0100

    Testing traduction variables

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
-    <section class="p-5 min-h-screen">
+    <section class="p-5">
       <h1 class="p-5 flex justify-center text-7xl">
         Welcome to the homepage
       </h1>
+      <h2>
+        {{ $t('social.twitter') }}
+      </h2>
     </section>
 </template>
