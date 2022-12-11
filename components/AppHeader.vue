eb5cf6e78d954dc60bded6b26bbcecae8312d9fb
alexruedadev <me@alexrueda.dev>
2022-12-11 17:29:47 +0100

    Added simple mobile menu

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="sticky top-0 z-10 backdrop-blur-md">
+    <header class="fixed w-full top-0 z-10 backdrop-blur-md">
       <nav class="max-w-7xl mx-auto navigation justify-between">
         <NuxtLink to="/">
           <img src="favicon.ico" alt="" class="w-10 h-10">
@@ -11,12 +11,30 @@
         </ul>
         <Switch class="hidden lg:flex"/>
         <!-- Burger icon -->
-        <NuxtLink class="flex lg:hidden">
+        <NuxtLink @click="toggleMobileMenu()" class="flex lg:hidden">
           <Icon name="mingcute:menu-line" class="text-gray-200 w-6 h-6 m-2" />
         </NuxtLink>
+
       </nav>
+
+      <MobileMenu class="hidden mobile-menu fadeIn" />
+
     </header>
   </template>
+
+<script>
+
+  export default {
+
+    methods: {
+      toggleMobileMenu(){
+        // Toggle mobile menu
+        document.querySelector('.mobile-menu').classList.toggle('hidden')
+      }
+    }
+  }
+
+</script>
   
 <style>
 .navigation {
