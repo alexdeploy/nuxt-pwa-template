c7a41f1c3659d578a353b87a01a4d57ce96f37b3
alexruedadev <me@alexrueda.dev>
2022-12-11 17:32:47 +0100

    Little changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    
-    <section class="flex max-w-7xl mx-auto p-5 justify-center">
+  <div class="flex">
+
+    <section class="flex max-w-7xl mx-auto p-5 justify-center h-screen items-center">
 
       <div>
         <span class="flex text-xl md:text-3xl justify-center">
           Welcome to
         </span>
-        <h1 class="text-4xl md:text-7xl font-sfbold">
+        <h1 class="flex text-4xl md:text-7xl font-sfbold justify-center">
           {{ $t('title') }}
         </h1>
-        <h2 class="flex text-xl md:text-2xl justify-end m-2">
+        <h2 class="flex text-xl md:text-2xl justify-center m-2">
           template
         </h2>
-
-        <libraryButtonNuxt value="About" to='/about' />
+        <div class="flex justify-center">
+          <libraryButtonNuxt value="About" to='/about' class="m-2"/>
+          <libraryButtonNuxt value="Get Started" to='/about' class="m-2"/>
+        </div>
       </div>
 
     </section>
